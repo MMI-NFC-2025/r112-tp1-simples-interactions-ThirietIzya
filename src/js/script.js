36b72ba8ff43829506a099d4ac31a807d171ac8a
ThirietIzya <izya.thiriet@edu.univ-fcomte.fr>
2025-11-07 16:13:22 +0100

    simple inter

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,15 @@
 // TODO
-constaMettreEnrouge = document.querySelector('#a-mettre-en-rouge')
-constaMettreEnrouge.style.color="red"
-constenRougeSuiteAclick = document.querySelector('#en-rouge-suite-a-click')
-constenRougeSuiteAclick.addEventListener ("click", (evt)=>{
-    constenRougeSuiteAclick.style.color='red'
+const aMettreEnrouge = document.querySelector('#a-mettre-en-rouge')
+aMettreEnrouge.style.color="red"
+
+const enRougeSuiteAclick = document.querySelector('#en-rouge-suite-a-click')
+enRougeSuiteAclick.addEventListener ("click", (evt)=>{
+    enRougeSuiteAclick.style.color='red'
 });
-constcollH2 = document.querySelectorAll ("h2")
-constcollH2.forEach ((elm)=>{elm.addEventListener('click',(evt)=>{ 
+
+const collH2 = document.querySelectorAll ("h2")
+collH2.forEach ((elm)=>{
+    elm.addEventListener('click',(evt)=>{ 
         evt.target.style.color = "red";
 });
 });
