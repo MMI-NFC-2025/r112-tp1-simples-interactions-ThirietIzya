8d0aa0acb3cd7c6b7a987d9a5a4fe1ae9306cb8f
ThirietIzya <izya.thiriet@edu.univ-fcomte.fr>
2025-11-07 15:57:09 +0100

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // TODO
+constaMettreEnrouge = document.querySelector('#a-mettre-en-rouge')
+constaMettreEnrouge.style.color="red"
+constenRougeSuiteAclick = document.querySelector('#en-rouge-suite-a-click')
+constenRougeSuiteAclick.addEventListener ("click", (evt)=>{
+    constenRougeSuiteAclick.style.color='red'
+});
+constcollH2 = document.querySelectorAll ("h2")
+cconstollH2.forEach ((elm)=>{elm.addEventListener('click',(evt)=>{ 
+        evt.target.style.color = "red";
+});
+});
